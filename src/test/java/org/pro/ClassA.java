4b580a8563a71d5cf1b1b073697a92dcc6c51522
Chitradevig <chituvicha@gmail.com>
2023-01-23 13:08:47 +0530

    toady's task

---
+++ src/test/java/org/pro/ClassA.java
@@ -0,0 +1,14 @@
+package org.pro;
+
+public class ClassA {
+private void TN() {
+	System.out.println("Many languages");
+
+}
+private void UP() {
+	System.out.println("Many languages");
+}
+private void AP() {
+	System.out.println("Many languages");
+}
+}
